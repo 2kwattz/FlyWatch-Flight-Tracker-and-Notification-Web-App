0bcf6b3190880bd813207cbf33e5a9b1166d4036
Roshan Bhatia <37065373+2kwattz@users.noreply.github.com>
2024-04-15 12:10:54 +0530

    Update flywatch.py

--- cli-script/flywatch.py
+++ cli-script/flywatch.py
@@ -1,1 +1,12 @@
+# Fly Watch Python CLI based script 
+# Code AUthor : Roshan Bhatia. Instagram : @2kwattz . Github : 2kwattz
 
+import pyfiglet
+import requests
+
+sources = {
+    'ADBS_EXCHANGE' : '',
+    'RADARBOX' : 'https://www.radarbox.com/',
+    'FR24' : '',
+    'AviationStack' : 'https://aviationstack.com/'
+}
