0bb6425807c342301bd333b5f62cc56528b0c91a
Roshan Bhatia <37065373+2kwattz@users.noreply.github.com>
2024-05-04 10:03:08 +0530

    Update flywatch.py

--- cli-script/flywatch.py
+++ cli-script/flywatch.py
@@ -1,12 +1,30 @@
 # Fly Watch Python CLI based script 
-# Code AUthor : Roshan Bhatia. Instagram : @2kwattz . Github : 2kwattz
-
+# Code Author : Roshan Bhatia. Instagram : @2kwattz . Github : 2kwattz
 import pyfiglet
 import requests
 
-sources = {
+api_sources = {
     'ADBS_EXCHANGE' : '',
     'RADARBOX' : 'https://www.radarbox.com/',
     'FR24' : '',
     'AviationStack' : 'https://aviationstack.com/'
 }
+
+# Local Databases
+
+class C17:
+    def __init__(self,registration,model,country):
+        self.model = model
+        self.registration = registration
+        self.country = country
+
+# Initialize Models
+
+VUAUA = C17("C17","VUAUA","India")
+VUAUB = C17("C17","VUAUB","India")
+VUAUD = C17("C17","VUAUD","India")
+VUAUH = C17("C17","VUAUH","India")
+VUAUJ = C17("C17","VUAUJ","India")
+VUAUK = C17("C17","VUAUK","India")
+VUAUL = C17("C17","VUAUL","India")
+
